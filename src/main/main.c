d37a3b0f23056d0576208e8a8f2defeafb4d7746
Corentin Mounier <mounie_c@epita.fr>
2019-06-27 18:15:35 +0200

    MPI: initialize mpi

--- src/main/main.c
+++ src/main/main.c
@@ -1,14 +1,30 @@
-#include <stdio.h>
-#include "utils.h"
 #include "cli.h"
+#include "utils.h"
+
+#include <mpi.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
-    // Parse and Work on bin arguments
-    (void) argc;
-    (void) argv;
+    // Initialize MPI
+    int rank, size, len;
+    char version[MPI_MAX_LIBRARY_VERSION_STRING];
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Get_library_version(version, &len);
+
+    // TODO: Parse and Work on bin arguments
 
     // Start CLI
-    start_cli();
+    if (rank == 0) {
+        printf("starting %d processes\n", size);
+        start_cli();
+    }
+    else
+    {
+        // TODO: Je sais pas je suis senser faire quoi
+    }
 
+    MPI_Finalize();
     return 0;
 }
