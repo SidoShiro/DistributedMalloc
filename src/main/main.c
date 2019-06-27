75fa5f8b1cbc52889e403963dce6982e3ef25148
sidore_m <marc@sidorenko.fr>
2019-06-27 18:43:57 +0200

    main: add comments for later

--- src/main/main.c
+++ src/main/main.c
@@ -8,6 +8,15 @@ int main(int argc, char **argv) {
     // Initialize MPI
     int rank, size, len;
     char version[MPI_MAX_LIBRARY_VERSION_STRING];
+
+    // Load Graph Here
+
+    // Generate Adjacency Matrix Here
+    void *adj_mat_network = NULL;
+    void *list_id_size = NULL;
+    (void) adj_mat_network;
+    (void) list_id_size;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -23,6 +32,23 @@ int main(int argc, char **argv) {
     else
     {
         // TODO: Je sais pas je suis senser faire quoi
+
+        // Node Creation
+        // Form rank number !
+
+        // Start Leader !
+
+
+        // Wait Leader
+
+        // If is leader
+
+
+
+        // else
+
+
+
     }
 
     MPI_Finalize();
