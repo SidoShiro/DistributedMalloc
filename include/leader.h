1ab85b9ea5461430376afa2a2b4e348faf590278
sidore_m <marc@sidorenko.fr>
2019-06-28 16:36:05 +0200

    leader: add leader struct: blocks allocation index, command_queue

--- include/leader.h
+++ include/leader.h
@@ -1,4 +1,16 @@
 #ifndef DISTRIBUTEDMALLOC_LEADER_H
 #define DISTRIBUTEDMALLOC_LEADER_H
 
+#include <stdlib.h>
+
+#include "node.h"
+#include "message.h"
+#include "command_queue.h"
+#include "block.h"
+
+struct leader {
+    struct blocks leader_blks;
+    struct command_queue leader_command_queue;
+};
+
 #endif /* !DISTRIBUTEDMALLOC_LEADER_H */
