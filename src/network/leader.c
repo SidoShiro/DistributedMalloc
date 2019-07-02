184393414923c5c65d6697031468c39017367bb9
sidore_m <marc@sidorenko.fr>
2019-07-02 18:19:47 +0200

    leader: add generate_leader_resources function

--- src/network/leader.c
+++ src/network/leader.c
@@ -1,4 +1,13 @@
 #include "node.h"
 #include "leader.h"
+#include "globals.h"
+#include <mpi.h>
+
+struct leader_resources *generate_leader_resources() {
+    struct leader_resources *l_r = malloc(sizeof(struct leader_resources));
+    l_r->leader_blks = NULL;
+    l_r->leader_command_queue = NULL;
+    return l_r;
+}
 
 
