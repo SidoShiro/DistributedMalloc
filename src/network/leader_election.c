00fabbfa57dd56055eed28c3a8e27c80e6e0455d
Corentin Mounier <mounie_c@epita.fr>
2019-07-02 23:22:45 +0200

    leader_election: fix election

--- src/network/leader_election.c
+++ src/network/leader_election.c
@@ -9,37 +9,42 @@
 unsigned leader_election(unsigned id, unsigned network_size) {
 
     unsigned new_leader = id;
-    unsigned leader = id;
+    unsigned leader = id + 1;
     unsigned next = next_id(id, network_size);
     struct message *m = NULL;
 
     while (1) {
-        if (new_leader <= leader) {
+        if (new_leader < leader) {
             leader = new_leader;
-            m = generate_message(id, next, id, leader, 0, OP_LEADER);
+            m = generate_message(id, next, leader, 0, 0, OP_LEADER);
             MPI_Send(m, sizeof(*m), MPI_BYTE, next, 201, MPI_COMM_WORLD);
-        
-            // free(m); ?
+            free(m);
         }
 
         MPI_Recv(m, sizeof(*m), MPI_BYTE, MPI_ANY_SOURCE, 201, MPI_COMM_WORLD,
             MPI_STATUS_IGNORE);
+
         if (m->op == OP_OK)
             break; // Should be replaced by a OP_LEADER_OK
-        if (m->op == OP_LEADER && m->id_o == id) {
-            // send OK
-            m = generate_message(id, 666, id, leader, 0, OP_OK); // does it really needs to be in the loop ?
-            for (unsigned i = 1; i < network_size; ++i) {
-                MPI_Send(m, sizeof(*m), MPI_BYTE, i, 201, MPI_COMM_WORLD);
-            }
-            // free(m);
 
-            m = generate_message(id, 666, id, leader, 0, OP_OK);
-            MPI_Send(m, sizeof(*m), MPI_BYTE, 0, 201, MPI_COMM_WORLD);
-            break;
+        if (m->op == OP_LEADER) {
+            new_leader = m->id_o;
+            if (m->id_o == id) {
+                // send OK
+                m = generate_message(id, 666, leader, 0, 0, OP_OK);
+                for (unsigned i = 1; i < network_size; ++i) {
+                    MPI_Send(m, sizeof(*m), MPI_BYTE, i, 201, MPI_COMM_WORLD);
+                }
+                free(m);
+
+                // send leader to user
+                m = generate_message(id, 666, leader, 0, 0, OP_OK);
+                MPI_Send(m, sizeof(*m), MPI_BYTE, 0, 201, MPI_COMM_WORLD);
+                free(m);
+                break;
+            }
         }
     }
-
     return leader;
 }
 
