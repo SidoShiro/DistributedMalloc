dba22ad4cea9728f6b7fbf41c2e10ba0b2395aac
Corentin Mounier <mounie_c@epita.fr>
2019-07-03 17:20:54 +0200

    handle-timeout: fix send_safe_message + implement receive_safe

--- include/communication.h
+++ include/communication.h
@@ -3,18 +3,19 @@
 
 #include "message.h"
 
-// Minimum waiting time before considering that the node is dead (ms)
-#define TIMEOUT 5000
+// Number of attempts failed before considering the node as dead.
+#define NB_ITER 6
 
 /*
  * Send the given message 'm' to the destination node
  * Return 1 if it's a success, 0 if the destination node didn't respond in time
  */
-int send_safe_message(struct message *m);
+int send_safe_message(struct message *m_send);
 
 /*
- *
+ * Receive a message sent with 'send_safe_message'
+ * then send a OP_OK to the source node
  */
-void receive_safe_message(struct message *m);
+void receive_safe_message(struct message *m_recv);
 
 #endif /* !DISTRIBUTEDMALLOC_COMMUNICATION_H */
--- src/network/communication.c
+++ src/network/communication.c
@@ -4,39 +4,55 @@
 #include <mpi.h>
 #include <stdio.h>
 
-int send_safe_message(struct message *m) {
-    MPI_Request r;
-    MPI_Isend(m, sizeof(struct message), MPI_BYTE, m->id_t, 0, MPI_COMM_WORLD, &r);
+int send_safe_message(struct message *m_send) {
+    MPI_Request r_send;
+    int f_send = 0;
+    MPI_Isend(m_send, sizeof(struct message), MPI_BYTE, m_send->id_t, 0, MPI_COMM_WORLD, &r_send);
 
-    clock_t start = clock();
-    clock_t last = start;
+    struct message m_ok;
+    MPI_Request r_ok;
+    int f_ok = 0;
+    MPI_Irecv(&m_ok, sizeof(struct message), MPI_BYTE, m_send->id_t, 0, MPI_COMM_WORLD, &r_ok);
+
     struct timespec waiting_time;
+    waiting_time.tv_sec = 0;
     waiting_time.tv_nsec = 10000000;
-    int flag = 0;
-    //struct message m_ok;
 
-    //MPI_Irecv(&m_ok, sizeof(struct message), MPI_BYTE, )
-
-    while ((start - last) / (double) CLOCKS_PER_SEC * 1000 < TIMEOUT) {
-        printf("waiting for %d\n", m->id_t);
+    for (int i = 0; i < NB_ITER; i++) {
+        printf("waiting for %d\n", m_send->id_t);
         fflush(0);
 
-        MPI_Test(&r, &flag, MPI_STATUS_IGNORE);
-        if (flag) {
-            printf("SUCCESS\n");
-            return 1;
+        if (!f_send) {
+            MPI_Test(&r_send, &f_send, MPI_STATUS_IGNORE);
+        } else {
+            printf("SEND OK\n");
+            fflush(0);
+            MPI_Test(&r_ok, &f_ok, MPI_STATUS_IGNORE);
+            if (f_ok) {
+                printf("SUCCESS\n");
+                fflush(0);
+                return m_ok.op == OP_OK;
+            }
         }
+        printf("start sleep\n");
+        fflush(0);
         nanosleep(&waiting_time, NULL);
+        printf("end sleep\n");
+        fflush(0);
         waiting_time.tv_nsec *= 2;
-        last = clock();
     }
-    printf("node %d TIMEOUT\n", m->id_t);
+    printf("node %d TIMEOUT\n", m_send->id_t);
     fflush(0);
     return 0;
 }
 
-void receive_safe_message(struct message *m)
-{
-    // TODO
-    (void) m;
+void receive_safe_message(struct message *m_recv) {
+    MPI_Status status;
+    MPI_Recv(m_recv, sizeof(struct message), MPI_BYTE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+    printf("RECEIVE SUCCESS\n");
+    fflush(0);
+    struct message *m_send = generate_message(m_recv->id_t, m_recv->id_s, 0, 0, 0, OP_OK);
+    MPI_Send(m_send, sizeof(struct message), MPI_BYTE, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+    free(m_send);
+    printf("SEND SUCCESS\n");
 }
