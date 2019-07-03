7b0db14c4e783c22188375dfffb9e0b3261e0c25
sidore_m <marc@sidorenko.fr>
2019-07-03 22:05:07 +0200

    block: change struct block node_/virtual_(address), add functions and fixes

--- include/block.h
+++ include/block.h
@@ -1,13 +1,23 @@
 #ifndef DISTRIBUTEDMALLOC_BLOCK_H
 #define DISTRIBUTEDMALLOC_BLOCK_H
 
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * size of block
+ * address on id node
+ * address virtual (user view)
+ * next block
+ * free : 0 is free, 1 is allocated
+ */
 struct block {
     unsigned short id;
     size_t size;
-    size_t address;
+    size_t node_address;
+    size_t virtual_address;
     struct block *next;
+    char free;
 };
 
 struct blocks {
@@ -15,10 +25,17 @@ struct blocks {
     struct block **blks;
 };
 
+struct blocks *init_nodes_same_size(unsigned short nb_nodes, size_t size);
+
 struct blocks *generate_blocks(size_t nb_blks);
 
-struct block *generate_block(unsigned short id, size_t size, size_t address);
+struct block *generate_block(unsigned short id, size_t size, size_t node_address, size_t virtual_address, char free);
 
-void add_block(struct block blk, unsigned short id, size_t size, size_t address);
+void
+add_block(struct block *blk, unsigned short id, size_t size, size_t node_address, size_t virtual_address, char free);
+
+void merge_free_block(struct blocks *blks);
+
+struct blocks *init_nodes_same_size(unsigned short nb_nodes, size_t size);
 
 #endif /* !DISTRIBUTEDMALLOC_BLOCK_H */
--- src/network/block.c
+++ src/network/block.c
@@ -8,17 +8,35 @@ struct blocks *generate_blocks(size_t nb_blks) {
     return blks;
 }
 
-struct block *generate_block(unsigned short id, size_t size, size_t address) {
+struct block *generate_block(unsigned short id, size_t size, size_t node_address,
+        size_t virtual_address, char free) {
     struct block *blk = malloc(sizeof(struct block));
     blk->id = id;
     blk->size = size;
-    blk->address = address;
+    blk->node_address = node_address;
+    blk->virtual_address = virtual_address;
+    blk->free = free;
     blk->next = NULL;
     return blk;
 }
 
-void add_block(struct block blk, unsigned short id, size_t size, size_t address) {
-    struct block *n_blk = generate_block(id, size, address);
-    blk.next = n_blk;
+void add_block(struct block *blk, unsigned short id, size_t size, size_t node_address,
+               size_t virtual_address, char free) {
+    struct block *n_blk = generate_block(id, size, node_address, virtual_address, free);
+    blk->next = n_blk;
     (void) blk;
+}
+
+void merge_free_block(struct blocks *blks);
+
+struct blocks *init_nodes_same_size(unsigned short nb_nodes, size_t size) {
+    struct blocks *blks = generate_blocks(nb_nodes);
+    size_t  v_address = 0;
+    for (size_t i = 0; i < blks->nb_blocks; i++) {
+        // Skip 0 (user), so from 1 to nb_nodes + 1
+        blks->blks[i] = generate_block(i + 1, size, 0, v_address, 0);
+        v_address += size;
+    }
+    printf("Global init has memory size of %zu bytes", v_address);
+    return blks;
 }
