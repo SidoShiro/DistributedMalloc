6138ceb7016f95b52cb54cd2348993ebe7703e0a
sidore_m <marc@sidorenko.fr>
2019-07-05 22:18:32 +0200

    test: add test_main, update CMakeList

---
+++ test/main_test.c
@@ -0,0 +1,18 @@
+#include "cli.h"
+#include "graph.h"
+#include "utils.h"
+#include "node.h"
+#include "leader.h"
+#include "globals.h"
+#include "debug.h"
+#include "leader_election.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
+    
+    
+}
