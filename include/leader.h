5b6f4542019773cfdc17bd6e288715aaee03b4f5
sidore_m <marc@sidorenko.fr>
2019-07-07 22:51:19 +0200

    leader: add id, generate_leader_resources

--- include/leader.h
+++ include/leader.h
@@ -12,7 +12,7 @@ struct leader_resources {
     struct block_register *leader_blks;
     struct allocation_register  *leader_reg;
     struct command_queue *leader_command_queue;
-    // unsigned short id;
+    unsigned short id;
 };
 
 struct address_search {
@@ -22,6 +22,8 @@ struct address_search {
     size_t n_address;
     size_t size;
 };
+
+struct leader_resources *generate_leader_resources(size_t nb_nodes , size_t id);
 
 void leader_loop(struct node *n, unsigned short terminal_id, unsigned short nb_nodes);
 
