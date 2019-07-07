3ccf61f623d037f5a2e8b663da5935c3fade513d
sidore_m <marc@sidorenko.fr>
2019-07-07 22:52:34 +0200

    utils: add print_allocations_table function def

--- include/utils.h
+++ include/utils.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "leader.h"
+
 struct a {
     int x;
     int y;
@@ -11,5 +13,7 @@ struct a {
 int fun();
 
 size_t len(char **array);
+
+void print_allocations_table(struct leader_resources *l_r);
 
 #endif //DISTRIBUTEDMALLOC_UTILS_H
