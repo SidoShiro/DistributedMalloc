ad3d16a80a4bc5eb2613c458a55f9615b1fc7124
sidore_m <marc@sidorenko.fr>
2019-07-10 15:22:08 +0200

    leader: add leader_utils, implement dead list functions

--- include/leader.h
+++ include/leader.h
@@ -1,12 +1,27 @@
 #ifndef DISTRIBUTEDMALLOC_LEADER_H
 #define DISTRIBUTEDMALLOC_LEADER_H
 
+#include <communication.h>
+#include <mpi.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include "node.h"
-#include "message.h"
-#include "command_queue.h"
 #include "block.h"
+#include "command_queue.h"
+#include "debug.h"
+#include "globals.h"
+#include "leader.h"
+#include "message.h"
+#include "node.h"
+#include "utils.h"
+
+struct dead_list {
+    size_t max;
+    unsigned short *dead_list;
+};
 
 struct leader_resources {
     struct block_register *leader_blks;
@@ -15,14 +30,7 @@ struct leader_resources {
     unsigned short id;
     size_t availaible_memory;
     size_t max_memory;
-};
-
-struct address_search {
-    unsigned short id;
-    size_t r_address;
-    size_t v_address;
-    size_t n_address;
-    size_t size;
+    struct dead_list *dead_list;
 };
 
 struct leader_resources *generate_leader_resources(size_t nb_nodes , size_t id);
@@ -32,5 +40,9 @@ void leader_loop(struct node *n, unsigned short terminal_id, unsigned short nb_n
 struct allocation *give_for_v_address(struct leader_resources *l_r, size_t v_address, size_t *part);
 
 size_t size_of_allocation(struct allocation *a);
+
+void add_dead(struct leader_resources *l_r, unsigned short id);
+
+void rm_dead(struct leader_resources *l_r, unsigned short id);
 
 #endif /* !DISTRIBUTEDMALLOC_LEADER_H */
